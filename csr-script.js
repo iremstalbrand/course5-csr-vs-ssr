f9919f1b9d60b0cd276e19c630f8bf982204ef83
Irem Stalbrand <iremstalbrand@gmail.com>
2026-02-23 18:08:36 +0100

    feat: feat: add article and footer elements in csr-script.js

--- csr-script.js
+++ csr-script.js
@@ -23,7 +23,7 @@ section.appendChild(sectionTitle);
 
 // ============ NAV ============
 
-const listElements = ["All", "Technology", "Design", "Business"];
+const listElements = ["All", "Technology", "Design", "Culture", "Business"];
 const nav = document.querySelector("nav");
 const ul = document.createElement("ul");
 nav.appendChild(ul);
@@ -39,3 +39,90 @@ listElements.forEach((item) => {
   li.appendChild(span);
   ul.appendChild(li);
 });
+
+// ============ ARTICLE ============
+
+const cards = [
+  {
+    category: "TECHNOLOGY",
+    title: "Quiet Tools That Work",
+    content:
+      "The best tools often go unnoticed, quietly making life easier. They handle small tasks in the background so you can focus on what matters most.",
+    date: "Feb 18, 2026",
+  },
+  {
+    category: "DESIGN",
+    title: "Small Details Matter",
+    content:
+      "A subtle shadow or soft curve can make a design feel alive. Small imperfections give warmth and character that perfect symmetry can’t match.",
+    date: "Feb 15, 2026 ",
+  },
+  {
+    category: "CULTURE",
+    title: "Alone, but Connected",
+    content:
+      "   Even in a crowd, quiet moments like reading or listening to music can create a small sense of connection and reflection.",
+    date: "Feb 5, 2026",
+  },
+  {
+    category: "BUSINESS",
+    title: "The New Shape of Work",
+    content:
+      "Careers rarely follow a straight path. People switch roles and learn new skills, creating opportunities for growth in unexpected ways.",
+    date: "Feb 2, 2026",
+  },
+];
+
+const article = document.querySelector("article");
+const articleContainer = document.createElement("div");
+articleContainer.classList.add("cards-container");
+
+cards.forEach((item) => {
+  const card = document.createElement("div");
+  card.classList.add("card");
+  const category = document.createElement("h2");
+  category.textContent = item.category;
+  const title = document.createElement("h3");
+  title.textContent = item.title;
+  const content = document.createElement("p");
+  content.textContent = item.content;
+
+  const cardFooter = document.createElement("div");
+  cardFooter.classList.add("card-footer");
+  const date = document.createElement("span");
+  date.textContent = item.date;
+  const readMore = document.createElement("span");
+  readMore.textContent = "Read more";
+  readMore.classList.add("read-more");
+
+  cardFooter.appendChild(date);
+  cardFooter.appendChild(readMore);
+  card.appendChild(category);
+  card.appendChild(title);
+  card.appendChild(content);
+  card.appendChild(cardFooter);
+  articleContainer.appendChild(card);
+});
+article.appendChild(articleContainer);
+
+// ============ FOOTER ============
+const footer = document.querySelector("footer");
+
+const footerContainer = document.createElement("div");
+footerContainer.classList.add("render-badge-ssr");
+
+const badge = document.createElement("div");
+badge.classList.add("badge-dot");
+
+const badgeText = document.createElement("span");
+badgeText.textContent = "Client-Side Rendered";
+
+const link = document.createElement("a");
+link.href = "ssr.html";
+link.classList.add("switch-link");
+link.textContent = "View SSR version →";
+
+footerContainer.appendChild(badge);
+footerContainer.appendChild(badgeText);
+footer.appendChild(footerContainer);
+footer.appendChild(link);
